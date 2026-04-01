b22195efcbdddac24d9bc0860cbcff7e9450b1b2
Lorena E <lorenasimarro4@gmail.com>
2026-04-01 20:44:52 +0200

    Update select.sql

--- sql/select.sql
+++ sql/select.sql
@@ -4,11 +4,11 @@ SELECT * FROM form WHERE name is NULL ;
 SELECT * FROM form WHERE name LIKE 'A%';
 SELECT * FROM form WHERE age BETWEEN 20 AND 30;
 SELECT * FROM form 
-WHERE subject = 'job' AND subject = 'information';
+WHERE subject IN = ('job', 'information');
 SELECT * FROM form 
 WHERE name = 'Maria' OR name = 'Ana';
 SELECT * FROM form 
-WHERE email = 'gmail' AND subject = 'job';
+WHERE email LIKE '%gmail%' AND subject = 'job';
 SELECT * FROM form 
-WHERE age > 30 AND message = 'test';
+WHERE age > 30 AND message LIKE '%test%';
 SELECT * FROM form ORDER BY email ASC;
